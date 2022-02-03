682048c2390bc88a30c3bd030d7c62c76b9fe808
Krzysztof Siek <krzysztof.siek01@gmail.com>
2022-02-03 20:07:12 +0100

    fix load images on production

--- src/app/image/image.component.ts
+++ src/app/image/image.component.ts
@@ -8,6 +8,7 @@ import { LettersService } from '../services/letters.service';
 })
 export class ImageComponent implements OnInit {
   wrongAnswer = 'empty';
+  imagesLoad = new Array();
 
   constructor(private letterService: LettersService) { }
 
@@ -15,6 +16,21 @@ export class ImageComponent implements OnInit {
     this.letterService.wrongAnswers.subscribe((res) => {
       this.wrongAnswer = `wrong${res}`;
     });
+    this.pload(
+      '/assets/hangman/1.png',
+      '/assets/hangman/2.png',
+      '/assets/hangman/3.png',
+      '/assets/hangman/4.png',
+      '/assets/hangman/5.png',
+      '/assets/hangman/6.png'
+    );
+  }
+
+  pload(...args: any[]): void {
+    for (let i = 0; i < args.length; i++) {
+      this.imagesLoad[i] = new Image();
+      this.imagesLoad[i].src = args[i];
+    }
   }
 
 }
